5774c3ab5d6beca1e454dbcf336bc268c59cc832
Kendric <181333067+kendricscoles@users.noreply.github.com>
2025-05-13 19:51:40 +0200

    Update room_type.py

--- model/room_type.py
+++ model/room_type.py
@@ -13,7 +13,13 @@ class RoomType:
 
     @property
     def room_type_id(self): return self.__room_type_id
+
     @property
     def description(self): return self.__description
+    @description.setter
+    def description(self, value): self.__description = value
+
     @property
     def max_guests(self): return self.__max_guests
+    @max_guests.setter
+    def max_guests(self, value): self.__max_guests = value
